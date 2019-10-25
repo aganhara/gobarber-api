fd5011690dd71f617fec9d62f51b95af2d988814
Anderson Ganhara <aganhara@gmail.com>
2019-10-24 21:35:48 -0300

    Adding avatar for users.
    Created migrations;
    linked avatar with users;
    Created a static link to provide images;
    Created File and Provider controller.
    Added muter on routes. Used to upload images.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -58,6 +58,7 @@ class UserController {
 
     const { email, oldPassword } = req.body;
     const user = await User.findByPk(req.userId);
+    console.log(user);
 
     if (email !== user.email) {
       const userExists = await User.findOne({
