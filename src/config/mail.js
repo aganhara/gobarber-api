f5d52cfb6b714ca7f64493ebc9a158829c28bf94
Anderson Ganhara <aganhara@gmail.com>
2019-10-27 13:10:17 -0300

    Adding nodemailer to send email.
    Using Mailtrap.io in dev environment, to test email send.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '8aee1da4b7c427',
+    pass: '8db71cd0ad6064',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
